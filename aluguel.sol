717b45a5f3f7949bd1123a2c2908bc384b9f252f
Marcello Acar <marcelloacar@users.noreply.github.com>
2024-02-07 06:39:56 +0900

    Update aluguel.sol

--- aluguel.sol
+++ aluguel.sol
@@ -1,6 +1,6 @@
 /*
 SPDX-License-Identifier: CC-BY-4.0
-(c) Desenvolvido por Jeff Prestes
+(c) Developed by Marcello Acar
 This work is licensed under a Creative Commons Attribution 4.0 International License.
 */
 
